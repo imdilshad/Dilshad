9f8ed659e9f68beb081446d9ac25d0efe7e7050d
Dilshad khan <152077382+imdilshad@users.noreply.github.com>
2024-01-29 20:30:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+let back_to_top=document.querySelector("#back-to-top")
+
+let lalMirch=document.querySelector("#lal-mirch")
+let haldi=document.querySelector("#haldi")
+let dhaniya=document.querySelector("#dhaniya")
+let chaipatti=document.querySelector("#chaipatti")
+
+lalMirch.addEventListener("click",()=>{
+  window.open("https://www.flipkart.com/raunak-rajasthani-dry-red-chilli-whole-sabut-laal-mirch-500g/p/itmbb962f2e1e2f8")
+})
+haldi.addEventListener("click",()=>{
+  window.open("https://www.flipkart.com/r-d-d-masala-rajasthani-turmeric-powder/p/itm1d5ae138eb48f")
+})
+dhaniya.addEventListener("click",()=>{
+  window.open("https://www.amazon.in/JK-Rajasthani-Dhaniya-Coriander-Powder/dp/B099Z3F8LZ")
+})
+chaipatti.addEventListener("click",()=>{
+  window.open("https://www.amazon.in/gp/aw/d/B08HK1MDXC/?_encoding=UTF8&pd_rd_plhdr=t&aaxitk=6b777875c1feb7f9c417908982394406&hsa_cr_id=0&qid=1706524888&sr=1-2-e0fa1fdd-d857-4087-adda-5bd576b25987&ref_=sbx_be_s_sparkle_mcd_asin_1_img&pd_rd_w=ihiSU&content-id=amzn1.sym.df9fe057-524b-4172-ac34-9a1b3c4e647d%3Aamzn1.sym.df9fe057-524b-4172-ac34-9a1b3c4e647d&pf_rd_p=df9fe057-524b-4172-ac34-9a1b3c4e647d&pf_rd_r=AMT86M8T4734VBHGS6HR&pd_rd_wg=S991E&pd_rd_r=cf1bf257-d4fd-4097-ac56-2020de0d041a")
+})
+
+
+
+back_to_top.addEventListener("click",()=>{
+  location.assign("index.html");
+})
+let insta=document.querySelector("#insta");
+let facebook=document.querySelector("#facebook");
+let youtube=document.querySelector("#youtube");
+insta.addEventListener("click",()=>{
+  window.open("https://www.instagram.com/imdil.shad/")
+})
+facebook.addEventListener("click",()=>{
+  window.open("https://www.instagram.com/imdil.shad/")
+})
+youtube.addEventListener("click",()=>{
+  window.open("https://www.youtube.com/@Hifimunde");
+})
